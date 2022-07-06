85fdd55978fa78a761ad181c23e60ff5e7876171
Jitendra Jawale <e002112@ip-10-192-240-215.eu-west-1.compute.internal>
2022-07-06 07:46:11 +0530

    Day6 nested for

---
+++ src/Day6.java
@@ -0,0 +1,141 @@
+public class Day6 {
+    public static void main(String[] args) {
+
+        /**
+         * patterrns using nested for
+         */
+
+
+        /*
+
+         *
+         **
+         ***
+         ****
+         *****
+
+
+         */
+
+        for (int line = 1; line <= 5; line++) {
+            for (int column = 1; column <= line; column++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+        1
+        22
+        333
+        4444
+        55555
+         */
+        /*
+        1
+        12
+        123
+        1234
+        12345
+         */
+        /*
+        55555
+        4444
+        333
+        22
+        1
+         */
+        /*
+        54321
+        4321
+        321
+        21
+        1
+         */
+        /*
+            1
+           22
+          333
+         4444
+        55555
+         */
+
+
+
+
+
+
+
+
+        /*
+
+         *****
+         ****
+         ***
+         **
+         *
+
+         */
+        System.out.println("----------------------");
+        for (int line = 5; line >= 1; line--) {
+            for (int column = 1; column <= line; column++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+        /*
+
+        ....*
+        ...**
+        ..***
+        .****
+        *****
+
+
+         */
+
+        for (int line = 1; line <= 5; line++) { //1
+            for (int column = 1; column <= 5; column++) { //1
+                if (column <= 5 - line) { //4
+                    System.out.print(".");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+
+        /*
+
+        ....*
+        ...**
+        ..***
+        .****
+        *****
+
+         */
+
+        System.out.println("-------------------");
+        int total = 5;
+        for(int line = 1; line<=total; line++) {
+            for(int dots = 1; dots <= total - line;dots++) {
+                System.out.print(".");
+            }
+
+            for(int star = 1; star <=line ; star++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+
+
+
+    }
+}
