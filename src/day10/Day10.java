ff9f477dbd1760c9c2dc0254dd8446df36a725b5
Jitendra Jawale <e002112@ip-10-192-240-215.eu-west-1.compute.internal>
2022-07-13 08:21:26 +0530

    add day11 class

--- src/day10/Day10.java
+++ src/day10/Day10.java
@@ -3,52 +3,86 @@ package day10;
 public class Day10 {
 
 
+    //Create object
+
+    //class should be written in SentenceCase (S caps, C caps)
+    //variables should be in camelCase (camel lowercase, Case -> C -> caps) -> (starts with lowercase)
+    //functions should be in camelCase (Starts with lowercase)
+
+    //nikhil -> variable -> () nahi hai
+    //nikhil() -> function -> () hai
+
+    //Body -> class -> CamelCase
+
+    //tryThisOut() -> function -> () aur camelCase
+
+    //seeMe -> variable -> camelCase -> () nahi hai
+
+    //SeeMe -> class -> SentenceCase
+    //Day7 -> class -> SentenceCase
+
+    //class Day7
     public static void main(String[] args) {
-        //Create object
+        /**
+         * Create object to call methods from other class
+         */
+        //class variableName = new class();
+        NumberUtils numberUtils = new NumberUtils(); //creating a object here -> numberUtils is object
+        numberUtils.printHelloFromNumberUtils();
+//        System.out.println("result = " + numberUtils.add2Numbers(10, 20));
+//        System.out.println("result = " + numberUtils.add2Numbers(103, 210));
+        System.out.println("Multiplication result = " + numberUtils.multiply2Numbers(103, 210));
+//        System.out.println("numberUtils.varaible1 = " + numberUtils.varaible1);
+//        numberUtils.varaible1 = 50;
+//        System.out.println("numberUtils.varaible1 = " + numberUtils.varaible1);
 
-        //class should be written in SentenceCase (S caps, C caps)
-        //variables should be in camelCase (camel lowercase, Case -> C -> caps) -> (starts with lowercase)
-        //functions should be in camelCase (Starts with lowercase)
-
-        //nikhil -> variable -> () nahi hai
-        //nikhil() -> function -> () hai
-
-        //Body -> class -> CamelCase
-
-        //tryThisOut() -> function -> () aur camelCase
-
-        //seeMe -> variable -> camelCase -> () nahi hai
-
-        //SeeMe -> class -> SentenceCase
-        //Day7 -> class -> SentenceCase
-
-        //class Day7
-
-
-        //class objectName = new class();
-        NumberUtils numberUtils = new NumberUtils();
-        //here we are creating a new object
-        //numberUtils
+        numberUtils.varaible1 = 50;
+        numberUtils.varaible2 = 70;
+        System.out.println("numberUtils.addAllVariables() = " + numberUtils.addAllVariables());
 
 
 
-
+        NumberUtils numberUtils1 = new NumberUtils();
+//        System.out.println("number utils 1 " + numberUtils1.multiply2Numbers(10,20));
+//        System.out.println("numberUtils1.varaible1 = " + numberUtils1.varaible1);
+        System.out.println("numberUtils1.addAllVariables() = " + numberUtils1.addAllVariables());
+        numberUtils1.varaible1 = 80;
+        System.out.println("numberUtils1.addAllVariables() = " + numberUtils1.addAllVariables());
+        
         //Class variables
 
         //How to use one class into another
 
         //Constructor
-            //default constructor
-            //parameterized constructor
+        //default constructor
+        //parameterized constructor
 
         //Access specifiers - private, public
 
-        
-
-
-
-
-
 
     }
+
+
+    /**
+     * 1. Create a package named tryme
+     * 2. Create a Class named MainClass and write psvm
+     * 3. Create a class named UtilsClass
+     * 4. Add few number functions in UtilsClass
+     *      a. Add 2 numbers and return the result
+     *      b. Find the largest number from given 2 numbers and return the result
+     *      c. Create a class variable named number and assign to 7
+     *      d. Find the factorial of a class variable number
+     * 5. Create an object of UtilsClass in MainClass
+     * 6. Call function to add 2 numbers and print
+     * 7. Call function to find the largest number and print
+     * 8. Call function to find the factorial
+     * 9. Update the number to 5
+     * 10. Call function to find the factorial
+     */
+
+
+
+
+
+
 }
--- src/day10/NumberUtils.java
+++ src/day10/NumberUtils.java
@@ -2,11 +2,36 @@ package day10;
 
 public class NumberUtils {
 
+
+    public int varaible1 = 20;
+    public int varaible2 = 40;
     /**
      * Function to add 2 numbers return the result
      */
     public int add2Numbers(int number1, int number2) {
         return number1 + number2;
     }
+
+    public void printHelloFromNumberUtils() {
+        System.out.println("Printing from Number Utils");
+    }
+
+    /**
+     * function to multiply 2 numbers
+     * @param number1
+     * @param number2
+     * @return the result of multiplcation of 2 numbers
+     */
+    public int multiply2Numbers(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public int addAllVariables() {
+        return varaible1 + varaible2;
+    }
+
+//    public int findLargest(int number1, int number2) {
+//    //logic
+//    }
 
 }
