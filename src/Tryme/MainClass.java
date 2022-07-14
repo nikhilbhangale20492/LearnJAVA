8d91a47188dfa49ddd45f7c93f1cef0eca9d67ff
Nikhil <niksbhangale@gmail.com>
2022-07-14 07:45:10 +0530

    a

---
+++ src/Tryme/MainClass.java
@@ -0,0 +1,41 @@
+package Tryme;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        /*
+        5. Create an object of UtilsClass in MainClass
+         */
+        UtilsClass utilsClass = new UtilsClass();
+
+        System.out.println("Add 2 Numbers = " + utilsClass.add2Number(65, 35));
+
+        System.out.println("largest Number = " + utilsClass.findLargestNumber(69, 82));
+
+      //  System.out.println("Factorial of 7 is = " + utilsClass.findFactorial(7));
+
+
+
+    }
+
+
+
+}
+
+/**
+ * 1. Create a package named tryme
+ * 2. Create a Class named MainClass and write psvm
+ * 3. Create a class named UtilsClass
+ * 4. Add few number functions in UtilsClass
+ *      a. Add 2 numbers and return the result
+ *      b. Find the largest number from given 2 numbers and return the result
+ *      c. Create a class variable named number and assign to 7
+ *      d. Find the factorial of a class variable number
+ * 5. Create an object of UtilsClass in MainClass
+ * 6. Call function to add 2 numbers and print
+ * 7. Call function to find the largest number and print
+ * 8. Call function to find the factorial
+ * 9. Update the number to 5
+ * 10. Call function to find the factorial
+ */
---
+++ src/Tryme/UtilsClass.java
@@ -0,0 +1,43 @@
+package Tryme;
+
+public class UtilsClass {
+    /*
+     * a. Add 2 numbers and return the result
+     */
+    public int add2Number(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    /*
+     b. Find the largest number from given 2 numbers and return the result
+     */
+    public int findLargestNumber(int number1, int number2) {
+        if (number1 > number2) {
+            return number1;
+        } else {
+            return number2;
+        }
+    }
+
+    /*
+    c. Create a class variable named number and assign to 7
+     */
+    public int number = 7;
+
+    /*
+    d. Find the factorial of a class variable number (factorial of 7 = 7x6x5x4x3x2x1)
+     result * i = result
+     1 * 1 = 1
+     1 * 2 = 2
+
+
+     */
+    public int findFactorial(int num) {
+        int result = 1;
+        for (int i = 1; i <= num; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+}
