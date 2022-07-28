0e96e4d8c81c767dc1ebf73831d06c1e2cc776fd
Jitendra Jawale <e002112@LP001360.local>
2022-07-28 07:05:26 +0530

    add assignments

--- src/assignments/Basic.java
+++ src/assignments/Basic.java
@@ -39,7 +39,7 @@ public class Basic {
          * Print whether reminder of 378 when divided by 7 is even or odd
          */
 
-        
+
     }
 
 
---
+++ src/assignments/NumberAssignment.java
@@ -0,0 +1,26 @@
+package assignments;
+
+public class NumberAssignment {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Write a function to return the largest number out of given 2 numbers
+     */
+
+
+    /**
+     * Write a function to return the smallest number out of given 2 numbers
+     */
+
+    /**
+     * Write a function to add 3 given numbers and return the result
+     */
+
+    /**
+     * 
+     */
+
+}
