c735af0f73ac2ba715443b11e4d55bc2774278b3
Jitendra Jawale <e002112@ip-10-192-240-215.eu-west-1.compute.internal>
2022-07-28 22:47:09 +0530

    add for assignment

---
+++ src/assignments/ForAssignment.java
@@ -0,0 +1,84 @@
+package assignments;
+
+public class ForAssignment {
+
+    static int array[] = {81, 72, 54, 88, 102, 23, 10, 77, 54, 78};
+
+    public static void main(String[] args) {
+
+        /**
+         * Print all the numbers of the given array
+         */
+
+
+        /**
+         * Print if the number is even or odd for all the numbers in the given array
+         */
+
+        /**
+         * Count number of even numbers in the given array
+         */
+
+        /**
+         * Count number of odd numbers in the given array
+         */
+
+        /**
+         * Print all the numbers less than 75 in the given array
+         */
+
+        /**
+         * Print all the numbers that are even and less than 75 in the given array
+         */
+
+        /**
+         * Print all the numbers that are greater than 75 and are odd
+         */
+
+        /**
+         * Print "More even numbers than odd" if even numbers count is greater than odd
+         * or else print "More Odd numbers than even" if odd numbers count is greater than even
+         * if equal number of even and odd numbers then print "Equal number of odd and even numbers"
+         *
+         */
+
+        /**
+         * Print the sum of all the numbers in the array
+         */
+
+
+        /**
+         * Print sum of all even numbers in the array
+         */
+
+        /**
+         * Print sum of all odd numbers in the array
+         */
+
+        /**
+         * Print sum of all numbers in even position in the array
+         * like 0,2,4,6,8
+         */
+
+
+        /**
+         * Print sum of all numbers in odd position in the array
+         * like 1,3,5,7,9
+         */
+
+        /**
+         * Print sum of first five numbers in the array
+         */
+
+
+        /**
+         * Print sum of numbers from 3rd position to 6th position in the array
+         */
+
+        /**
+         * Print the duplicate number from the array
+         */
+
+    }
+
+}
