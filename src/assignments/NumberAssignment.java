0ecc6f0d2d385be4c268d205064aa50090ec41e7
Jitendra Jawale <e002112@LP001360.local>
2022-07-28 07:21:25 +0530

    add number assignment

--- src/assignments/NumberAssignment.java
+++ src/assignments/NumberAssignment.java
@@ -15,12 +15,72 @@ public class NumberAssignment {
      * Write a function to return the smallest number out of given 2 numbers
      */
 
+
     /**
      * Write a function to add 3 given numbers and return the result
      */
 
+
     /**
-     *
+     * Write a function to subract 2 given numbers and return the result
      */
+
+
+    /**
+     * Write a function to print all the even single digit numbers out of given number
+     * Eg: 87624
+     * 8
+     * 6
+     * 2
+     * 4
+     */
+
+
+    /**
+     * Write a function to sum all the numbers in a given number and return result
+     * Eg: 87053
+     * 8 + 7 + 0 + 5 + 3 = 23
+     */
+
+
+    /**
+     * Write a function to sum all the numbers in a given number until the sum is a single digit and return result
+     * Eg: 895678
+     * 8+9+5+6+7+8 = 43
+     * 4+3 = 7
+     */
+
+
+    /**
+     * Write a function to find the largest single digit out of given number and return result
+     * Eg: 97665
+     * 9 is the largest single digit number
+     */
+
+
+    /**
+     * Write a function that takes 5 integers as parameters and add only the even numbers and return result
+     * Eg: 87,46,97,16,82
+     * 46+16+82 = 144
+     */
+
+
+    /**
+     * Write a function to reverse any given number and return the result
+     */
+
+
+    /**
+     * Write a function to count number of 0's and 1's in a given binary number. (Consider binary number as String)
+     * Binary number will always contain 0's and 1's
+     * Eg: "0010010110", "101001101"
+     *
+     * 0010010110
+     * Count 0 = 6
+     * Count 1 = 4
+     */
+
+
+
 
 }
